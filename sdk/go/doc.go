fad7f05e709c08be1e900845c52c55229e00f979
David Driscoll <david.driscoll@gmail.com>
2023-12-01 17:01:33 -0500

    updated go package name

---
+++ sdk/go/doc.go
@@ -0,0 +1,2 @@
+// Package one-password-native-unoffical exports types, functions, subpackages for provisioning one-password-native-unoffical resources.
+package pulumi_one_password_native_unoffical
--- sdk/go/onepassword/doc.go
+++
@@ -1,2 +0,0 @@
-// Package one-password-native-unoffical exports types, functions, subpackages for provisioning one-password-native-unoffical resources.
-package onepassword
