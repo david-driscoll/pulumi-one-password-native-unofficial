0a595746a38e8ca41887cdbc1238f92881deaff2
David Driscoll <david.driscoll@gmail.com>
2023-12-26 20:54:01 -0500

    Disable logging as it might be causing some issues

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION         := 0.0.22
+VERSION         := 0.0.23
 
 PACK            := one-password-native-unofficial
 PROJECT         := david-driscoll/pulumi-one-password-native-unofficial
--- provider/cmd/pulumi-resource-one-password-native-unofficial/HostSink.cs
+++ provider/cmd/pulumi-resource-one-password-native-unofficial/HostSink.cs
@@ -17,13 +17,13 @@ class HostSink : ILogEventSink
 
     public async void Emit(LogEvent logEvent)
     {
-        var message = logEvent.RenderMessage(_formatProvider);
-        var urnString = "";
-        if (logEvent.Properties.TryGetValue("Urn", out var urn) || logEvent.Properties.TryGetValue("urn", out urn))
-        {
-            urnString = urn.ToString(null, _formatProvider);
-        }
-        await _host.LogAsync(new(GetLogSeverity(logEvent.Level), message, urnString)).ConfigureAwait(false);
+        // var message = logEvent.RenderMessage(_formatProvider);
+        // var urnString = "";
+        // if (logEvent.Properties.TryGetValue("Urn", out var urn) || logEvent.Properties.TryGetValue("urn", out urn))
+        // {
+        //     urnString = urn.ToString(null, _formatProvider);
+        // }
+        // await _host.LogAsync(new(GetLogSeverity(logEvent.Level), message, urnString)).ConfigureAwait(false);
         // _host.LogAsync(new(GetLogSeverity(logEvent.Level), message, "")).Wait();
     }
 
